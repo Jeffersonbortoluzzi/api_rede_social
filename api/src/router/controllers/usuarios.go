eddf6f3ed5eb3b034af95791ec9d7070f2e66871
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-03-02 10:04:25 -0300

    package respostas

--- api/src/router/controllers/usuarios.go
+++ api/src/router/controllers/usuarios.go
@@ -4,37 +4,42 @@ import (
 	"api/src/banco"
 	"api/src/modelos"
 	"api/src/repositorios"
+	"api/src/respostas"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 // CriarUsuario insere um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := ioutil.ReadAll(r.Body)
-	if erro != nil {
-		log.Fatal(erro)
+	erro = errors.New("Deu erro")
+	if erro != nil {    
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
 	}
 
 	var usuario modelos.Usuario
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
-	usuarioId, erro := repositorio.Criar(usuario)
+	usuario.ID, erro = repositorio.Criar(usuario)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioId)))
+	respostas.JSON(w, http.StatusCreated, usuario)
 }
 
 // BuscarUsuarios busta todos os usuários salvos no banco

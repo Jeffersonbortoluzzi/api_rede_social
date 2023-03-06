64b2d436ee89aeb02af186b26143083adc719cc1
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-03-06 11:41:15 -0300

    package Bcrypt

--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -1,6 +1,7 @@
 package modelos
 
 import (
+	"api/src/seguranca"
 	"errors"
 	"strings"
 	"time"
@@ -24,36 +25,50 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return erro
 	}
 
-	usuario.formatar()
+	if erro := usuario.formatar(etapa); erro != nil {
+		return erro
+	}
+
 	return nil
 }
 
 func (usuario *Usuario) validar(etapa string) error {
 	if usuario.Nome == "" {
-		return errors.New("O nome é obrigatório e não pode estar em branco.")
+		return errors.New("O nome é obrigatório e não pode estar em branco")
 	}
 
 	if usuario.Nick == "" {
-		return errors.New("O nick é obrigatório e não pode estar em branco.")
+		return errors.New("O nick é obrigatório e não pode estar em branco")
 	}
 
 	if usuario.Email == "" {
-		return errors.New("O e-mail é obrigatório e não pode estar em branco.")
+		return errors.New("O e-mail é obrigatório e não pode estar em branco")
 	}
 
 	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
-		return errors.New("O e-mail inserido é inválido.")
+		return errors.New("O e-mail inserido é inválido")
 	}
 
 	if etapa == "cadastro" && usuario.Senha == "" {
-		return errors.New("A senha é obrigatório e não pode estar em branco.")
+		return errors.New("A senha é obrigatória e não pode estar em branco")
 	}
 
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, erro := seguranca.Hash(usuario.Senha)
+		if erro != nil {
+			return erro
+		}
+
+		usuario.Senha = string(senhaComHash)
+	}
+
+	return nil
 }

9ccd7264f7556577ae0bba72ce8c2a158031d2e3
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-03-07 16:18:33 -0300

    validacao token

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -2,6 +2,10 @@ package autenticacao
 
 import (
 	"api/src/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -18,4 +22,37 @@ func CriarToken(usuarioID uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
 	return token.SignedString([]byte(config.SecretKey))
 
+}
+
+// ValidarToken confirma se o token é valido
+func ValidarToken(r *http.Request) error {
+	tokenString := extrairToken(r)
+	token, erro := jwt.Parse(tokenString, retornarChaveVerificacao)
+	if erro != nil {
+		return erro
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("Token inválido")
+}
+
+func extrairToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func retornarChaveVerificacao(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Método de assinatura não compativel. %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
--- api/src/middlewares/middlewares.go
+++ api/src/middlewares/middlewares.go
@@ -1,23 +1,29 @@
 package middlewares
 
 import (
+	"api/src/autenticacao"
+	"api/src/respostas"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 // Logger escreve dados da requisição dentro do terminal
-func Logger(next http.HandlerFunc) http.HandlerFunc {
+func Logger(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		proximaFuncao(w, r)
 	}
 }
 
 // Auntenticar verifíca se o usuario que fez a autenticação está autenticado
-func Auntenticar(next http.HandlerFunc) http.HandlerFunc {
+func Auntenticar(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Auntenticando")
-		next(w, r)
+		if erro := autenticacao.ValidarToken(r); erro != nil {
+			respostas.Erro(w, http.StatusUnauthorized, erro)
+			return
+		}
+		proximaFuncao(w, r)
 	}
 }

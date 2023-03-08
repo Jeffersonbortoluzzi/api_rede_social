ac90a1cee28f1036e086e630c7c0b08b56a81b48
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-03-08 11:46:31 -0300

    table seguidores

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS apirede;
 USE apirede;
 
+
+DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
@@ -8,8 +10,20 @@ CREATE TABLE usuarios(
     nome varchar(50) not null,
     nick varchar(50) not null unique,
     email varchar(50) not null unique,
-    senha varchar(100) not null unique,
+    senha varchar(100) not null,
     criadoEm timestamp default current_timestamp()
 ) ENGINE=INNODB;
- 
- 
+
+CREATE TABLE seguidores(
+    usuario_id int not null,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    seguidor_id int not null,
+    FOREIGN KEY (seguidor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    primary key(usuario_id, seguidor_id)
+) ENGINE=INNODB;

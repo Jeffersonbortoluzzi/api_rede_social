13366e4e3f1bc83ccec1767f265e57fac09b2229
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-03-09 14:31:44 -0300

    validacao de publicacao

--- api/src/modelos/publicacao.go
+++ api/src/modelos/publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa um publicação que o usuário fez
 type Publicacao struct {
@@ -11,4 +15,33 @@ type Publicacao struct {
 	AutorNick string    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+// Preparar chama os métodos para validar, formatar a publicação recebida
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O título é obrigatório e não pode estar em branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteúdo é obrigatório e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() error {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
+	return nil
 }

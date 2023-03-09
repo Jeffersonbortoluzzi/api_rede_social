de9363a96c1621dfe0b6ad394b53f7253a2b374d
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-03-09 18:10:10 -0300

    curtir e descurtir publicações

--- api/src/router/rotas/publicacoes.go
+++ api/src/router/rotas/publicacoes.go
@@ -37,9 +37,21 @@ var rotasPublicacoes = []Rota{
 		RequerAutenticacao: true,
 	},
 	{
-		URI:                "/usuarios/{usuariosId}/publicacoes",
+		URI:                "/usuarios/{usuarioId}/publicacoes",
 		Metodo:             http.MethodGet,
 		Funcao:             controllers.BuscarPublicacoesPorUsuario,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoId}/curtir",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CurtirPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoId}/descurtir",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.DescurtirPublicacao,
 		RequerAutenticacao: true,
 	},
 }
